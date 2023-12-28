476954db26e629b9f5878c37cda0e7504d396be9
Eirene <zu007kr@gmail.com>
2023-12-28 11:02:18 +0900

    dashboard

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,6 +1,7 @@
 
-import { OverviewCard, OverviewCardSkeleton }  from '@/ui/analytics/overview-cards';
+import { OverviewCard }  from '@/ui/analytics/overview-cards';
 import { MetricCardHolder }  from '@/ui/analytics/metric-cards';
+import { CardSkeleton } from "@/ui/skeletons";
 
 import { Suspense } from 'react';
 import { auth } from '@/auth';
@@ -12,7 +13,7 @@ export default async function Page() {
     <main className="p-0">
       <h2 className="mb-4 text-xl md:text-2xl">대시보드</h2>
       <div className="flex flex-col mb-6 md:flex-row md:justify-between">
-        <Suspense fallback={<OverviewCardSkeleton />}>
+        <Suspense fallback={<CardSkeleton />}>
           <OverviewCard session={session} />
         </Suspense>
       </div>
--- app/dashboard/projects/[id]/analytics/not-found.tsx
+++ app/dashboard/projects/[id]/analytics/not-found.tsx
@@ -6,12 +6,12 @@ export default function NotFound() {
     <main className="flex flex-col items-center justify-center h-full gap-2">
       <FaceFrownIcon className="w-10 text-gray-400" />
       <h2 className="text-xl font-semibold">404 Not Found</h2>
-      <p>Could not find the requested project.</p>
+      <p>요청한 프로젝트를 찾을 수 없습니다.</p>
       <Link
         href="/dashboard/projects"
         className="px-4 py-2 mt-4 text-sm text-white transition-colors bg-blue-500 rounded-md hover:bg-blue-400"
       >
-        Go Back
+        프로젝트 리스트로 돌아가기
       </Link>
     </main>
   );
