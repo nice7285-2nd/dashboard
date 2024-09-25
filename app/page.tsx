37d24b43cb85b8c54b3393f977bf0c2d8d0f9af5
nice7285 <nice7285@gmail.com>
2024-09-25 09:51:19 +0900

    제목 수정

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ import { shimmer } from '@/ui/animations';
 export default function Page() {
   return (
     <main className="flex flex-col min-h-screen p-4">
-      <div className={`${shimmer} relative overflow-hidden `}> 
-      {/* <div className="relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:bg-gradient-shimmer before:from-transparent before:via-white/60 before:to-transparent"> */}
+      <div className={`${shimmer} relative overflow-hidden `}>
+        {/* <div className="relative overflow-hidden before:absolute before:inset-0 before:-translate-x-full before:animate-shimmer before:bg-gradient-shimmer before:from-transparent before:via-white/60 before:to-transparent"> */}
         <div className="flex items-center h-20 p-2 bg-blue-500 rounded-lg">
           <LevelupLogo />
         </div>
@@ -16,7 +16,8 @@ export default function Page() {
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
         <div className="flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-3/5 md:px-20">
           <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
-            <strong>레벨업 대시보드에 오신걸 환영합니다.</strong> <br />Next.js 웹 애플리케이션 성능 분석을 위한 대시보드입니다.
+            <strong>환영합니다.</strong> <br />
+            Fishbone Workbooks를 통한 스터디 사이트 입니다.
           </p>
           <div className="flex gap-2">
             <Link
