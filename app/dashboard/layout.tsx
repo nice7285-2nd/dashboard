2b82483dc3e8171e6173f0c50a90607d06724c48
nice7285 <nice7285@gmail.com>
2024-09-27 07:32:12 +0900

    화살표방향수정

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="flex-none w-auto">
         <SideNav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto bg-slate-100">
+      <div className="flex-grow p-4 md:overflow-y-auto bg-slate-100">
         {children}
       </div>
     </div>
