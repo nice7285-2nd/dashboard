cb6f30afb50ed24f0de8da6e62c3803aa83ba999
Ubuntu <ubuntu@ip-172-31-30-12.ap-northeast-2.compute.internal>
2024-11-25 22:09:49 +0000

    change variable

--- auth.config.ts
+++ auth.config.ts
@@ -1,27 +1,25 @@
-import type { NextAuthConfig } from 'next-auth';
+import { NextAuthConfig } from 'next-auth';
 
-export const authConfig = {
+export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
+    authorized({ auth, request: { nextUrl } }: { auth: any; request: { nextUrl: URL } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        return false;
       } else if (isLoggedIn) {
         return Response.redirect(new URL('/dashboard/lessons', nextUrl));
       }
       return true;
     },
   },
-  providers: [], // 실제 provider는 auth.ts에서 설정됨
+  providers: [],
   trustHost: true,
   secret: process.env.NEXTAUTH_SECRET,
   basePath: "/api/auth",
-  url: "https://www.fsbone.com",
   debug: true,
-  allowedHosts: ["www.fsbone.com", "192.168.130.85"],
 };
