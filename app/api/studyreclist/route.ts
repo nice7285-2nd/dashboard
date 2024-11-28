bc7d7aee5e8d680920ba3cf0a93d494a5e1f2a92
Ubuntu <ubuntu@ip-172-31-30-12.ap-northeast-2.compute.internal>
2024-11-28 18:43:40 +0000

    수정

--- app/api/studyreclist/route.ts
+++ app/api/studyreclist/route.ts
@@ -7,12 +7,13 @@ export const revalidate = 0;
 
 export async function GET() {
   try {
-    const data = await prisma.studyreclist.findMany();
-    const processedData = data.map(video => ({
-      ...video,
-      fullUrl: `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com${video.path}`
-    }));
-    return NextResponse.json(processedData);
+    const result = await prisma.studyreclist.findMany({
+      orderBy: {
+        createdAt: 'desc'
+      }
+    });
+
+    return NextResponse.json(result);
   } catch (error) {
     console.error('Database Error:', error);
     return NextResponse.json(
