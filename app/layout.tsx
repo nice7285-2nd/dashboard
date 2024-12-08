c70e82fa70b7079d820f239c6c5caf2f50cf4cfa
Ubuntu <ubuntu@ip-172-31-30-12.ap-northeast-2.compute.internal>
2024-12-08 16:41:08 +0000

    불필요 파일 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,15 @@
 import '@/ui/globals.css';
 import { notoSansKR } from '@/ui/fonts';
 import { Metadata } from 'next';
-import { WebVitals } from '@/utils/web-vitals';
 import { SessionProvider } from "next-auth/react"
 
 export const metadata: Metadata = {
   title: {
-    // template: '%s | Dashboard',
     template: 'Fishbone | %s',
     default: 'Fishbone',
   },
   description: 'Fishbone',
-  metadataBase: new URL('https://www.fishbone.com/'),
+  metadataBase: new URL(process.env.NEXTAUTH_URL || 'http://localhost:3000'),
 };
 
 export default function RootLayout({
