6c696d13a87101c181c382d312b9d9024bceed7c
Ubuntu <ubuntu@ip-172-31-30-12.ap-northeast-2.compute.internal>
2024-12-08 16:03:03 +0000

    로그인 원복

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,7 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
 
-export function middleware(request: NextRequest) {
-  // 현재 호스트와 프로토콜 가져오기
-  const host = request.headers.get('host')
-  const protocol = request.headers.get('x-forwarded-proto') || 'http'
-  
-  // NEXTAUTH_URL을 동적으로 설정
-  process.env.NEXTAUTH_URL = `${protocol}://${host}`
-  
-  return NextResponse.next()
-}
+export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: [
