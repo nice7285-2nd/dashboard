bb94e0e250b8c79e4fc05d84efa591d8eb5c6672
Ubuntu <ubuntu@ip-172-31-30-12.ap-northeast-2.compute.internal>
2024-12-15 19:07:32 +0000

    모바일 타이틀 이미지 수정

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Page() {
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
-            src="/hero-mobile.png"
+            src="/fishbone-image.png"
             width={560}
             height={620}
             className="block md:hidden"
